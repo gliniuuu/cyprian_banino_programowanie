dbf06376fcb73f763785fd029377dcc5bb9251fa
gliniuuu <mateusz.gliniu@gmail.com>
2023-11-13 13:16:19 +0100

    komentarz

--- lekcja3 pętle.cpp
+++ lekcja3 pętle.cpp
@@ -2,36 +2,23 @@
 using namespace std;
 
 int main(){
-	/* inkrementacja
-	i = i + 1
-	i += 1
-	i++
-	i = i - 1
-	i -= 1
-	i --
-	*/
-	// for(wartoscpoczatkowa; warunekKonczacy; krok)
-	for (int i=0; i <= 5; i++){
-		cout << "wartosc i: " << i << endl; // I - ZMIENNA lokalna, dzial tylko w tej petli
-	}
-	
-	int wysokosc, szerokosc;
 
-	cout << "daj wysokosc";
-	cin >> wysokosc;
-	cout << "daj szerokosc";
-	cin >> szerokosc;
+	cout << "********************";
+	cout << endl;
 	
-	
-	
-	
-	for (int i=0; i <= szerokosc; i++){
-		for(int j=0; j<wysokosc; j++){
-			cout << " " << i << j;
-		}
+		cout << "*                        *";
 		cout << endl;
-	}
 	
+			cout << "*                              *";
+			cout << endl;
+		
+				cout << "*                              *";
+			cout << endl;
+		
+					cout << "********************";
+	
+	
+
 	
 cout << endl;
 system("pause");
---
+++ petle2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int wysokosc;
+int szerokosc;
+cout << "podaj wysokosc ";
+cin >> wysokosc;
+cout << "podaj szerokosc ";
+cin >>szerokosc;
+
+
+	for (int i=1; i <=wysokosc; i++){
+		for(int j=1; j<=szerokosc; j++){
+	
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+
+
+
+
+
+}
