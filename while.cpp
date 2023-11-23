ccd175a21fff621dcbdc8eae916c187a08f12a46
gliniuuu <mateusz.gliniu@gmail.com>
2023-11-23 11:03:05 +0100

    komentarz

---
+++ while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	short ;['PPPPPPPPPPPPPPPPP0']licznik = 30;
+	
+	while (licznik > 0){
+		cout << licznik << ", ";
+		licznik--;
+	}
+	
+	
+	
+	
+	
+	
+}
