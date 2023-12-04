52639e251ae53c4e287d6821bb9915faed567e97
gliniuuu <mateusz.gliniu@gmail.com>
2023-12-04 13:15:14 +0100

    komentarz

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[10];
+		for (int i = 0; i < 5; a++) {
+			cin >> a[i]
+				cout << a[i]; " ";
+
+
+		}
+		
+
+
+
+
+
+}
+
---
+++ tab.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+//zad 2
+//int liczba[ 5 ];
+//cout << "podaj 5 liczb ";
+//cout << endl;
+//for(int i=1; i <=5; i++)
+//{
+//  cout << "liczba numer " << i << "= ";
+//    cin >> liczba[ i ];
+//}
+
+
+//cout << "Podales nastepujace liczby: ";
+//for(int i=5; i >=1; i--)
+
+//    cout << liczba[ i ] << ", ";
+
+// zad 3
+//int liczba[ 5 ];
+//cout << "podaj 5 liczb ";
+//cout << endl;
+//for(int i=1; i <=5; i++)
+//{
+//    cin >> liczba[ i ];
+//}
+
+
+
+//cout << "Podales nastepujace liczby: ";
+
+//for(int i=1; i <=5; i++){
+
+//	if (liczba[ i ] < 0 ){
+	
+//	liczba[ i ] = liczba[ i ] * -1;
+//}
+//	cout << liczba[ i ] << ", ";
+
+//}
+
+
+//zad4
+int liczba[ 3 ][ 5 ];
+cout << "podaj 5 liczb ";
+cout << endl;
+for(int i=1; i <=5; i++)
+{
+ cout << "liczba numer " << i << "= ";
+   cin >> liczba[ i ];
+}
+
+
+cout << "Podales nastepujace liczby: ";
+for(int i=5; i >=1; i--)
+
+//    cout << liczba[ i ] << ", ";
+}
+
